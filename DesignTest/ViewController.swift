b32eff7dc2004119cfb2d33e4449f93b31e3bf9d
Marco Sautto <marcosautto@10-22-49-158.0200.par.it.iosda.org>
2018-09-20 13:05:23 +0200

    Main screen e settings, il logo per qualche motivo non si vede, devo risolvere.

--- DesignTest/ViewController.swift
+++ DesignTest/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logo: UIImageView!
+    
+    
     override func viewDidLoad() {
+
+        
+        let layer = CAGradientLayer()
+        layer.frame = view.bounds
+        layer.colors = [UIColor.red.cgColor, UIColor.purple.cgColor]
+        layer.startPoint = CGPoint(x: 0, y: 0)
+        view.layer.addSublayer(layer)
+        
+        
+        
+        self.title = "VibrApp"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationController?.navigationBar.barTintColor = view.backgroundColor
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        let textAttribute = [NSAttributedStringKey.foregroundColor:UIColor.red]
+        navigationController?.navigationBar.titleTextAttributes = textAttribute
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +40,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        
+        self.navigationController?.navigationBar.isHidden = false
+    }
 
 }
 
