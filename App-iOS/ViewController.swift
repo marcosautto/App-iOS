5486b2fe1a4e827d3022c9709c8208f167e85d02
Marco Sautto <marcosautto@par-102.local>
2018-09-20 18:52:51 +0200

    Applicazione da presentare

--- App-iOS/ViewController.swift
+++ App-iOS/ViewController.swift
@@ -9,15 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var logo: UIImageView!
+    
     override func viewDidLoad() {
+        
+        self.title = "VibrApp"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationController?.navigationBar.barTintColor = view.backgroundColor
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
+        
+//        let layer = CAGradientLayer()
+//        layer.frame = view.bounds
+//        layer.colors = [UIColor.red.cgColor, UIColor.purple.cgColor]
+//        layer.startPoint = CGPoint(x: 0, y: 0)
+//        view.layer.addSublayer(layer)
+        
+        
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        self.navigationController?.navigationBar.isHidden = false
     }
 
 
