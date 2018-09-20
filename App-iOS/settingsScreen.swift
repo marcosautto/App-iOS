7beb2f8b701046da294d430131ed399f8b89a563
Marco Sautto <marcosautto@par-102.local>
2018-09-20 18:57:47 +0200

    Applicazione da presentare

---
+++ App-iOS/settingsScreen.swift
@@ -0,0 +1,40 @@
+//
+//  settingsScreen.swift
+//  App-iOS
+//
+//  Created by Marco Sautto on 20/09/18.
+//  Copyright © 2018 musicaecoding. All rights reserved.
+//
+
+import UIKit
+
+class settingsScreen: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+
+}
