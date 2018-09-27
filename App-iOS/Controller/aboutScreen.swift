9e6da4fa3a5b518155a0ad02c7212245803ee75f
Luigi Borchia <luigib96@gmail.com>
2018-09-27 10:00:12 +0200

    about

--- App-iOS/Controller/aboutScreen.swift
+++ App-iOS/Controller/aboutScreen.swift
@@ -9,29 +9,28 @@
 import UIKit
 
 class aboutScreen: UIViewController {
+    
+    @IBOutlet weak var imgview: UIImageView!
 
     override func viewDidLoad() {
         self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
-        self.navigationController?.navigationBar.tintColor = UIColor.white
+//        self.navigationController?.navigationBar.tintColor = UIColor.white
         self.title = "About"
         self.navigationController?.navigationBar.prefersLargeTitles = true
-        self.navigationController?.navigationBar.tintColor = UIColor.white
+//        self.navigationController?.navigationBar.tintColor = UIColor.white
+        self.view.backgroundColor = UIColor.black
+        
+        if let theme = (UserDefaults.standard.object(forKey: "theme") as? String) {
+            if theme == "light" {
+                self.imgview.isHidden = false
+                self.navigationController?.navigationBar.tintColor = UIColor.white
+            } else {                                //Night Mode
+                self.imgview.isHidden = true
+                self.navigationController?.navigationBar.tintColor = UIColor.orange
+            }
+        }
         
         super.viewDidLoad()
     }
-    
-//    override func viewWillAppear(_ animated: Bool) {
-//
-//        if let theme = (UserDefaults.standard.object(forKey: "theme") as? String) {
-//            if theme == "light" {
-//                self.imgview.isHidden = false
-//            } else {                                //Night Mode
-//                self.imgview.isHidden = true
-//            }
-//        }
-//
-//        self.navigationController?.navigationBar.isHidden = false
-//    }
-    
     
 }
