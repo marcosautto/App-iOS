1b979c150aa7a20802d75e30dd544a5ca235d6a8
Andrea Lombardi <andrealombardi@par-138.local>
2018-09-28 10:37:38 +0200

    lmmmmm

--- App-iOS/Helpers/secondsToMinuteAndSeconds.swift
+++ App-iOS/Helpers/secondsToMinuteAndSeconds.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-
+//
 func secondsToHoursMinutesSeconds (inputSeconds : Int) -> String {
     let seconds = (inputSeconds % 3600) % 60
     let minutes = (inputSeconds % 3600) / 60
