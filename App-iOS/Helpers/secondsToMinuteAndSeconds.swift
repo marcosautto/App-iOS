9ab6a47add56942fb20e0c9bf92fc93c97bc3e13
Andrea Lombardi <andrealombardi@par-138.local>
2018-09-28 10:07:57 +0200

    added file to source control

---
+++ App-iOS/Helpers/secondsToMinuteAndSeconds.swift
@@ -0,0 +1,16 @@
+//
+//  secondsToMinuteAndSeconds.swift
+//  App-iOS
+//
+//  Created by Marco Sautto on 27/09/2018.
+//  Copyright © 2018 musicaecoding. All rights reserved.
+//
+
+import Foundation
+
+func secondsToHoursMinutesSeconds (inputSeconds : Int) -> String {
+    let seconds = (inputSeconds % 3600) % 60
+    let minutes = (inputSeconds % 3600) / 60
+    let timeString = ("\(minutes)"+":"+"\(seconds)")
+    return timeString
+}
